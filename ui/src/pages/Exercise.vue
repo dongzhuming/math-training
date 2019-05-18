858727f836feca57dfadde59371a03b6a851df5b
dongzhuming <dongzhuming@gmail.com>
2019-05-18 16:15:13 +0800

    调整题目显示弹出方式

--- ui/src/pages/Exercise.vue
+++ ui/src/pages/Exercise.vue
@@ -1,17 +1,18 @@
 <template>
   <q-page class="flex flex-center">
     <q-btn @click.native="popupModal()">准备好了，开始吧</q-btn>
-    <q-modal v-model="maximizedModal" :content-css="{minWidth: '30vw', minHeight: '30vh'}" no-esc-dismiss no-backdrop-dismiss>
+    <q-modal v-model="exerciseModal" :content-css="{minWidth: '30vw', minHeight: '30vh'}"
+             no-esc-dismiss no-backdrop-dismiss>
       <q-modal-layout>
         <q-toolbar slot="header">
-          <q-btn flat round dense v-close-overlay icon="keyboard_arrow_left"/>
+          <q-btn flat round dense @click.native="exitExercise()" icon="keyboard_arrow_left"/>
           <q-toolbar-title>
             剩余{{this.remain}}道题目
             <span slot="subtitle">
               已完成{{this.current}}道题目
             </span>
           </q-toolbar-title>
-          <q-btn flat @click.native="checkAnswer()">确定</q-btn>
+          <q-btn flat @click.native="checkAnswer()" >下一道 ></q-btn>
         </q-toolbar>
 
         <div class="layout-padding">
@@ -22,10 +23,11 @@
     </q-modal>
   </q-page>
 </template>
-<style>
-  .q-if {
-    font-size: 2rem
-  }
+<style lang="stylus">
+  .q-input
+    height 3em
+    font-size 3em
+
 </style>
 <script>
 import axios from 'axios'
@@ -33,14 +35,16 @@ export default {
   name: 'PageExercise',
   data () {
     return {
-      maximizedModal: false,
+      exerciseModal: false,
       questions: [],
       answers: [],
       answer: '',
       current: 0,
       remain: 10,
       inputPrefix: '',
-      inputSuffix: ''
+      inputSuffix: '',
+      penaltyCount: 5,
+      initialCount: 10
     }
   },
   methods: {
@@ -54,14 +58,14 @@ export default {
         this.remain--
         this.current++
         if (this.remain === 0) {
-          // TODO close modal
+          this.accomplish()
         } else {
           this.displayQuestion()
         }
       } else {
         // 答案错误，加10道
         this.$q.notify(`${this.questions[this.current].title} 的正确答案是: "${this.answers[this.current]}"`)
-        axios.get(`/api/exercise?count=10`).then(response => {
+        axios.get(`/api/exercise?count=${this.penaltyCount}`).then(response => {
           response.data.forEach(question => {
             this.questions.push(question)
           })
@@ -88,7 +92,7 @@ export default {
       this.inputSuffix = suffix
     },
     popupModal () {
-      axios.get(`/api/exercise?count=20`).then(response => {
+      axios.get(`/api/exercise?count=${this.initialCount}`).then(response => {
         this.questions = []
         response.data.forEach(question => {
           this.questions.push(question)
@@ -97,7 +101,27 @@ export default {
         this.remain = response.data.length
         this.current = 0
         this.displayQuestion()
-        this.maximizedModal = true
+        this.exerciseModal = true
+      })
+    },
+    exitExercise () {
+      this.$q.dialog({
+        title: '确定退出吗？',
+        message: '您还没有完成任务哦',
+        ok: '仍然退出',
+        cancel: '继续做完'
+      }).then(() => {
+        this.exerciseModal = false
+      }).catch(() => {
+      })
+    },
+    accomplish () {
+      this.$q.dialog({
+        title: '全部完成',
+        message: '总共完成{}道题目，其中有道错题',
+        ok: '知道了'
+      }).then(() => {
+        this.exerciseModal = false
       })
     }
   }
