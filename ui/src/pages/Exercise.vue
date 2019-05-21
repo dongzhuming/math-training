60b93a18efeacdb8e55f1842eaf5512f61591522
dongzhuming <dongzhuming@gmail.com>
2019-05-21 20:36:30 +0800

    增加题目数量选择

--- ui/src/pages/Exercise.vue
+++ ui/src/pages/Exercise.vue
@@ -1,23 +1,48 @@
 <template>
   <q-page class="flex flex-center">
-    <q-btn @click.native="popupModal()">准备好了，开始吧</q-btn>
+    <div class="row">
+      <p class="caption">
+        <span class="chip-container">
+            <q-chip square color="primary">
+              我要做 {{ initialCount }}道
+            </q-chip>
+          </span>
+      </p>
+      <q-slider v-model="initialCount" :min="5" :max="50" :step="5" color="blue" />
+
+      <p class="caption">
+        <span class="chip-container">
+          <q-chip square color="primary">
+            错一道加 {{ penaltyCount }}道
+          </q-chip>
+        </span>
+      </p>
+      <q-slider v-model="penaltyCount" :min="1" :max="10" :step="1" color="green" />
+      <q-btn @click.native="popupModal()">准备好了，开始吧</q-btn>
+    </div>
     <q-modal v-model="exerciseModal" :content-css="{minWidth: '30vw', minHeight: '30vh'}"
              no-esc-dismiss no-backdrop-dismiss>
       <q-modal-layout>
         <q-toolbar slot="header">
-          <q-btn flat round dense @click.native="exitExercise()" icon="keyboard_arrow_left"/>
+          <q-btn flat round dense @click.native="exitExercise()" icon="keyboard_arrow_left"></q-btn>
           <q-toolbar-title>
             剩余{{this.remain}}道题目
             <span slot="subtitle">
               已完成{{this.current}}道题目
             </span>
           </q-toolbar-title>
-          <q-btn flat @click.native="checkAnswer()" >下一道 ></q-btn>
+          <q-btn flat @click.native="checkAnswer()" >下一题</q-btn>
         </q-toolbar>
-
-        <div class="layout-padding">
-          <q-input type="number" v-model="answer" :prefix="inputPrefix" :suffix="inputSuffix"
-                   @keyup.enter="checkAnswer()"></q-input>
+        <div class="row layout-padding">
+          <div class="title">
+            <span>{{inputPrefix}}</span></div>
+          <div>
+            <q-input type="number" v-model="answer"
+                     @keyup.enter="checkAnswer()"/>
+          </div>
+          <div class="title">
+            <span>{{inputSuffix}}</span>
+          </div>
         </div>
       </q-modal-layout>
     </q-modal>
@@ -25,10 +50,15 @@
 </template>
 <style lang="stylus">
 .q-input
+  width 100px
+input[type="number"]
   font-size 2em
-  height 2em
-input[placeholder]
-  text-overflow ellipsis
+  height 40px
+.title
+  padding-top 7px
+  padding-bottom 7px
+  font-size 2em
+
 </style>
 <script>
 import axios from 'axios'
@@ -111,6 +141,7 @@ export default {
         title: '确定退出吗？',
         message: '您还没有完成任务哦',
         ok: '仍然退出',
+        color: 'primary',
         cancel: '继续做完'
       }).then(() => {
         this.exerciseModal = false
